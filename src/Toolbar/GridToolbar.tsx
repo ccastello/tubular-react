09d58375523930103328e6763daa05bd5e9831e1
cloc <ccastello@athmo.eu>
2020-11-26 12:15:23 +0100

    add option gridFeatures

--- src/Toolbar/GridToolbar.tsx
+++ src/Toolbar/GridToolbar.tsx
@@ -107,7 +107,7 @@ export const GridToolbar: React.FunctionComponent<GridToolbarProps> = ({
                     />
                 )}
 
-                {enableFeaturesDrawer && (
+                {enableFeaturesDrawer && toolbarOptions.gridFeatures && (
                     <Tooltip title={labelGridFeatures}>
                         <IconButton aria-label="Grid features" onClick={togglePanel}>
                             <TuneIcon />
